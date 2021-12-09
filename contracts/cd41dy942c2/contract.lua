31117400a689b9cd0820f89f28e3cd87dff1452c
etleneum <etleneum@hulsmann>
2021-12-09 12:27:17 -0500

    contract cd41dy942c2 deleted.

--- contracts/cd41dy942c2/contract.lua
+++
@@ -1,8 +0,0 @@
-function __init__ ()
-  return {}
-end
-
-function tipme ()
-  contract.send('020b73b23976bd7dacf3ba5cd7f8d7412995a94afa243b4f9f7b6f321d428e2882', call.msatoshi)
-  util.print('Thanks!')
-end
